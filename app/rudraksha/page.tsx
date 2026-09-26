818140098324387210ace24c90e44282dd775a3d
chavananand02340-wq <chavananand02340@gmail.com>
2026-09-26 14:05:15 +0530

    Update page.tsx

--- app/rudraksha/page.tsx
+++ app/rudraksha/page.tsx
@@ -1,5 +1,18 @@
-<CategoryPage
-        title="Rudraksha"
-        description="Sacred Rudraksha rooted in traditional Indian practices and spiritual heritage."
+import Header from "@/components/Header";
+import CategoryPage from "@/components/CategoryPage";
+import { getProductsByCategory } from "@/lib/getProducts";
+
+export default async function RudrakshaPage() {
+  const categoryProducts = await getProductsByCategory("rudraksha");
+
+  return (
+    <>
+      <Header />
+
+      <CategoryPage
+        slug="rudraksha"
         products={categoryProducts}
       />
+    </>
+  );
+}
